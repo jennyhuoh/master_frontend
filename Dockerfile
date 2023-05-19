aff64ab5ffad5ed810e00ee84c25bda9a9e4ff0d
jennyhuoh <huoh.li@gmail.com>
2023-05-19 21:18:17 +0800

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
-FROM node:18.12.1
-WORKDIR /app
-ADD . /app
+FROM node:18.12.1 AS builder
+WORKDIR /usr/src/app
+COPY package*.json ./
 RUN yarn install
+COPY . .
+RUN yarn build
+
+#Stage 2
+#######################################
+FROM node:18.12.1
+WORKDIR /build
+COPY --from=builder /usr/src/app/build .
+RUN npm i -g serve
 EXPOSE 3000
-CMD yarn start
+CMD serve -s build
--- src/features/api/index.js
+++ src/features/api/index.js
@@ -1,32 +1,32 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL:'http://localhost:3001/',
-    baseURL: 'http://140.115.126.21:3001',
-    // baseURL: 'https://ocdbackend-jennyhuoh.koyeb.app/',
+    baseURL:'http://localhost:8000/',
+    // baseURL: 'http://140.115.126.21:3001',
+    // baseURL: 'https://a7d1-111-248-29-162.ngrok-free.app',
     headers: {
         'X-Powered-By':'Express',
         'Content-Type':'application/json',
         'Access-Control-Allow-Origin':'*',
         'Access-Control-Allow-Methods':'POST, GET, PUT, DELETE',
         'Access-Control-Max-Age': 26400,
-        // "ngrok-skip-browser-warning": true,
-        // Origin: window.location.origin
+        "ngrok-skip-browser-warning": true,
+        Origin: window.location.origin
     },
     timeout:20000,
 })
 const recordInstance = axios.create({
-    // baseURL: 'http://localhost:3001/',
-    baseURL: 'http://140.115.126.21:3001',
-    // baseURL: 'https://ocdbackend-jennyhuoh.koyeb.app/',
+    baseURL: 'http://localhost:8000/',
+    // baseURL: 'http://140.115.126.21:3001',
+    // baseURL: 'https://a7d1-111-248-29-162.ngrok-free.app',
     headers: {
         'X-Powered-By': 'Express',
         'Content-Type': 'multipart/form-data',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE',
         'Access-Control-Max-Age': 26400,
-        // "ngrok-skip-browser-warning": true,
-        // Origin: window.location.origin
+        "ngrok-skip-browser-warning": true,
+        Origin: window.location.origin
     },
     timeout: 20000,
 })
@@ -53,35 +53,35 @@ instance.interceptors.response.use(
 )
 
 // User Login
-export const userLogin = data => instance.post('/login', data);
+export const userLogin = data => instance.post('/api/login', data);
 // create or get a user
-export const createUser = data => instance.post('/register', data);
+export const createUser = data => instance.post('/api/register', data);
 // create a group
-export const createGroup = data => instance.post('/group', data);
+export const createGroup = data => instance.post('/api/group', data);
 // create a stage
 export const createStage = async (data) => {
-    const response = await instance.post('/stage', data)
+    const response = await instance.post('/api/stage', data)
     const datas = await response.data;
     return datas;
 };
 // Create new teams in a stage
 export const createTeams = async ({id, teams}) => {
-    const response = await instance.post(`/stage/${id}/teams`, teams);
+    const response = await instance.post(`/api/stage/${id}/teams`, teams);
     const data = await response.data;
     return data;
 }
 // Create a new activity
 export const createActivity = async ({id, activity}) => {
-    const response = await instance.post(`/group/${id}/activity`, activity);
+    const response = await instance.post(`/api/group/${id}/activity`, activity);
     const data = await response.data;
     return data;
 }
 // Create a record
-export const createRecord = ({stageId, teamId, data}) => recordInstance.post(`stage/${stageId}/team/${teamId}/record`, data);
+export const createRecord = ({stageId, teamId, data}) => recordInstance.post(`/api/stage/${stageId}/team/${teamId}/record`, data);
 
 // get all users
 export const getAllUsers = async() => {
-    const response = await instance.get('/users')
+    const response = await instance.get('/api/users')
     const data = await response.data;
     var result = []
     await Promise.all(data.map((user) => {
@@ -98,32 +98,32 @@ export const getAllUsers = async() => {
 };
 // get all groups
 export const getAllGroups = async (userId) => {
-    const response = await instance.get(`/groups/${userId}`)
+    const response = await instance.get(`/api/groups/${userId}`)
     const data = await response.data;
     return data;
 }
 // get a group info
 export const getGroupInfo = async(groupId) => {
-    const response = await instance.get(`/group/${groupId}`)
+    const response = await instance.get(`/api/group/${groupId}`)
     const data = await response.data;
     return data;
 }
 // get teams for a stage
 export const getTeams = async (id) => {
-    const response = await instance.get(`/stage/${id}/teams`);
+    const response = await instance.get(`/api/stage/${id}/teams`);
     const data = await response.data;
     const teams = await data.teams;
     return teams;
 }
 // Get activities from a group
 export const getActivities = async (id) => {
-    const response = await instance.get(`/group/${id}/activities`);
+    const response = await instance.get(`/api/group/${id}/activities`);
     const data = await response.data;
     return data
 }
 // Get recordings
 export const getRecordings = async ({stageId, teamId}) => {
-    const response = await recordInstance.get(`/stage/${stageId}/team/${teamId}/records`);
+    const response = await recordInstance.get(`/api/stage/${stageId}/team/${teamId}/records`);
     const data = await response.data;
     console.log('getRecording response', data);
     let result = [];
@@ -143,48 +143,48 @@ export const getRecordings = async ({stageId, teamId}) => {
 }
 // Get an activity
 export const getAnActivity = async (id) => {
-    const response = await instance.get(`/activity/${id}`);
+    const response = await instance.get(`/api/activity/${id}`);
     const data = await response.data;
     return data;
 }
 // Get single user's activities
 export const getUserActivities = async(id) => {
-    const response = await instance.get(`/${id}/activities`);
+    const response = await instance.get(`/api/${id}/activities`);
     const data = await response.data;
     return data;
 }
 // Get usable teamTemplates
 export const getTemplates = async({id, userId}) => {
     // console.log('userId', userId)
-    const response = await instance.get(`/group/${id}/${userId}/activity/stage/teamTemplate`);
+    const response = await instance.get(`/api/group/${id}/${userId}/activity/stage/teamTemplate`);
     const data = await response.data;
     return data
 }
 
 // Delete a stage
-export const deleteStage = (id, data) => instance.delete(`/stage/${id}`, data);
+export const deleteStage = (id, data) => instance.delete(`/api/stage/${id}`, data);
 // delete an activity
-export const deleteActivity = (id, data) => instance.delete(`/activity/${id}`, data);
+export const deleteActivity = (id, data) => instance.delete(`/api/activity/${id}`, data);
 // delete a group
-export const deleteGroup = (id, data) => instance.delete(`/group/${id}`, data);
+export const deleteGroup = (id, data) => instance.delete(`/api/group/${id}`, data);
 
 // Update a group
 export const updateGroup = async ({id, groupInfo}) => {
-    const response = await instance.post(`/group/${id}`, groupInfo);
+    const response = await instance.post(`/api/group/${id}`, groupInfo);
     const data = await response.data;
     return data;
 }
 // Save stages sequence
-export const saveStagesSequence = (data) => instance.post('/stages', data);
+export const saveStagesSequence = (data) => instance.post('/api/stages', data);
 // Edit a stage
 export const editStage = async ({id, stage}) => {
-    const response = await instance.post(`/stage/${id}`, stage);
+    const response = await instance.post(`/api/stage/${id}`, stage);
     const data = await response.data;
     return data
 };
 // Update an activity
 export const updateActivity = async ({id, info}) => {
-    const response = await instance.post(`/activity/${id}`, info);
+    const response = await instance.post(`/api/activity/${id}`, info);
     const data = await response.data;
     return data
 }
